8a14b966340a227a7f36a4bbca97deaf83574bc4
liuxiongzhuo <3231729413@qq.com>
2025-01-03 13:20:17 +0800

    sadf

--- README.md
+++ README.md
@@ -3,19 +3,29 @@
 
 该项目从 https://github.com/beilunyang/img-mom 产生 , 去除了 oss 的设置 , 也不需要 kv 和 d2 存储 , 主打的就是一个简陋 , 代码很大部分来源于 img-mom , 再次表示感谢 .
 
-项目原理:
+项目工具栈 :
+
+typescript , grammy , hono
+
+项目原理 :
 
 用户在 tg 客户端上传图片给 bot , 搭建在 cloudflare workers 上面的 hono 以 webhooks 的方式运行 tg bot api , 读取文件并返回链接 , 链接的主要内容是文件的 file_id . hono 的 get 路由读取到 file_id , cloudflare workers 作为中介返回文件 .
 
-项目特点:
+项目特点 :
 
 足够简陋 , 容易部署
 
-未来开发走向:
+未来开发走向 :
 
 网页端上传 , 图片备份
 
-效果图:
+效果图 :
 
 ![](./images/1.png)
 ![](./images/2.jpg)
+
+部署方式 :
+
+cloudflare :
+
+clone 该项目 , 运行 npm install , 复制 src/config.example.json 到 config.json , 在 config.json 里面填上自己的域名 , url 前缀 , 运行 npm run deploy .
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,19 @@
+// 导入模块
 import { Bot, webhookCallback } from "grammy";
 import { Hono } from 'hono'
 import { fileTypeFromBuffer } from 'file-type'
 
-
+// 导入配置
 import config from "./config.json";
 const host = config.host
 const tgBotToken = config.tgBotToken
 const position = config.position
 
-
-
+// 新建 hono 和 bot
 const app = new Hono()
 const bot = new Bot(tgBotToken)
 
-
-
+// bot 监听 photo 和 document
 bot.on(":photo", async (ctx) => {
   const photos = ctx.msg.photo
   const fileId = photos[photos.length-1].file_id
@@ -25,12 +24,10 @@ bot.on(":document", async (ctx) => {
   await ctx.reply(`直链:\n<code>${host}/${position}/${fileId}</code>\nmarkdown:\n<code>![${fileId}]/(${host}/${position}/${fileId}/)</code>`,{parse_mode:"HTML"})
 })
 
-
-
+// hono 接受 tg bot 的 webhooks
 app.post('/bot', webhookCallback(bot, 'hono'));
-app.get('/', (ctx) =>
-  ctx.text('hello')
-)
+
+// hono 接受图片 file_id
 app.get(`/${position}/:fileId`, async (ctx) => {
   const fileId = ctx.req.param('fileId')
   const filePath = (await bot.api.getFile(fileId)).file_path
@@ -45,4 +42,6 @@ app.get(`/${position}/:fileId`, async (ctx) => {
     'Content-Type': fileType?.mime ?? '',
   });
 })
+
+// 导出 app
 export default app
